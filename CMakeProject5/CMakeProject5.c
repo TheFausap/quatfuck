0320e9dea9399130c2daf48f63b5afd5beabfdc1
Fausto Saporito <fausto.saporito@gmail.com>
2023-12-13 20:34:45 +0100

    Add project files.

---
+++ CMakeProject5/CMakeProject5.c
@@ -0,0 +1,53 @@
+﻿// CMakeProject5.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject5.h"
+
+int d2t(int v)
+{
+	int r = (v % 4) + 4;
+	int j = 10;
+	v /= 3;
+
+	for(;v;j*=10,v/=3) r += ((v % 4) + 4)*j;
+
+	return r;
+}
+
+int t2d(int t)
+{
+	int r = 0, rt = 0;
+	int j = 1;
+
+	for (;t;j*=4,t/=10) { rt = (t % 10) - 4; r += rt * j; }
+
+	return r;
+}
+
+int rol(int t)
+{
+	int d = log(t) / log(10);
+	d++;
+	int l = 0, r = t;
+	if (d > 1)
+	{
+		l = d % 10;
+		r -= l;
+		r += (l * d);
+	}
+
+	return r;
+}
+
+I main(I n, C **a)
+{
+	Od(d2t(0));
+	Od(d2t(4));
+	Od(d2t(5));
+	Od(d2t(10));
+	Od(d2t(17339));
+	Od(t2d(56));
+	Od(t2d(rol(56)));
+	Od(t2d(3));
+	R 0;
+}
---
+++ CMakeProject5/CMakeProject5.h
@@ -0,0 +1,15 @@
+﻿// CMakeProject5.h : Include file for standard system include files,
+// or project specific include files.
+
+
+typedef int I;
+typedef char C;
+
+#define R return
+#define O printf
+#define Od(x) printf("%d\n",x)
+
+#include <stdio.h>
+#include <math.h>
+
+// TODO: Reference additional headers your program requires here.
